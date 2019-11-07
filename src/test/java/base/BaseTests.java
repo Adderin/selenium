76bec3064f4af3acefc992648ee080d22239b548
Adderin <adderin@gmail.com>
2019-11-07 21:51:27 +0100

    new

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,29 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BaseTests {
+    private WebDriver driver;
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+        driver.findElement(By.linkText("Shifting Content")).click();
+        driver.findElement(By.linkText("Example 1: Menu Element")).click();
+        List<WebElement> lis = driver.findElements(By.tagName("li"));
+        System.out.println("Numbers of elements in menu: " + lis.size());
+
+        //System.out.println(driver.getTitle());
+        driver.quit();
+    }
+    public static void main (String args[]) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
