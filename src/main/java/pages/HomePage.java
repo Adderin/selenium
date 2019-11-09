9b16debff045e18150647ea7a5a952fef484e5e0
Adderin <adderin@gmail.com>
2019-11-09 17:53:24 +0100

    adding dropdown

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
-    private WebDriver driver;
-    private By formAuthentificationLink = By.linkText("Form Authentication");
+
+        private WebDriver driver;
 
     public HomePage(WebDriver driver){
             this.driver = driver;
     }
     public LoginPage clickFormAuthentification() {
-            driver.findElement(formAuthentificationLink).click();
+            clickLink("Form Authentication");
             return new LoginPage(driver);
+    }
+    public DropdownPage clickDropDown() {
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+
+    private void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
     }
 }
